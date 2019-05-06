414db95c0fb01de4288852fb9a050b4af5a93a52
常珂珂 <changkeke@sinosoft.com.cn>
2019-05-06 11:31:13 +0800

    v-for练习

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/info">Info</router-link>
+      <router-link to="/lianxi">lianxi</router-link>
     </div>
     <router-view/>
   </div>
--- src/views/Lianxi.vue
+++ src/views/Lianxi.vue
@@ -1,13 +1,47 @@
 <template>
-<h1></h1>
+  <div>
+    <ul>
+      <li v-for="(item,index) in showList"
+          @click="choose(index)"
+          :class="{activity: index==current && current !== '' }"
+          :key="index"
+          >
+        {{item}}
+      </li>
+    </ul>
+    <button type="button" @click="add()">添加</button>
+    <ul>
+      <li v-for="(item,index) in targetList" :key="index">{{item}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Lianxi'
+  name: 'Lianxi',
+  data: function () {
+    return {
+      current: '',
+      showList: [1, 2, 3, 4, 5, 6, 7, 8],
+      targetList: []
+    }
+  },
+  methods: {
+    choose (index) {
+      this.current = index
+      console.log(this.current)
+    },
+    add () {
+      if (this.current === '') return
+      this.targetList.push(this.showList[this.current])
+      this.current = ''
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+    .activity{
+      background: greenyellow;
+    }
 </style>
