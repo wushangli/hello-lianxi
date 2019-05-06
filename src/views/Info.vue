b0f9409917de4d96fae6e260e4b177581cd0ea53
常珂珂 <changkeke@sinosoft.com.cn>
2019-05-06 10:37:21 +0800

    router练习

---
+++ src/views/Info.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+     <h1>
+       Hello Info!!!
+     </h1>
+      <button type="type" @click="add()">添加</button>
+    </div>
+</template>
+
+<script>
+
+import store from '@/store.js'
+export default {
+  name: 'Info',
+  comments: {
+    store
+  },
+  methods: {
+    add: function () {
+      console.log('This is Info')
+      store.commit('increat')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
