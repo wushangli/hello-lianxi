777d890ff3f562c7df77a179b3154488b178c20b
常珂珂 <changkeke@sinosoft.com.cn>
2019-05-06 10:18:50 +0800

    提交

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
-<template>
+  <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{msg}}</p>
   </div>
 </template>
+<script>
+import store from '@/store.js'
+export default {
+  name: 'about',
+  store,
+  data: function () {
+    return {
+      msg: store.state.count
+    }
+  }
+}
+</script>
